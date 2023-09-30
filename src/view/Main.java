975f3549e56cfd164e4f18fffda18d903440ef65
thanhhieu1515 <144198175+thanhhieu1515@users.noreply.github.com>
2023-09-30 10:50:08 +0700

    Add files via upload

---
+++ src/view/Main.java
@@ -0,0 +1,32 @@
+package view;
+
+import model.Country;
+import controller.ManageEastAsiaCountries;
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<Country> lc = new ArrayList<>();
+       
+        while (true) {
+            int choice = ManageEastAsiaCountries.menu();
+            switch (choice) {
+                case 1:
+                    ManageEastAsiaCountries.inputCountry(lc);
+                    break;
+                case 2:
+                    ManageEastAsiaCountries.printCountry(lc);
+                    break;
+                case 3:
+                    ManageEastAsiaCountries.searchByName(lc);
+                    break;
+                case 4:
+                    ManageEastAsiaCountries.printCountrySorted(lc);
+                    break;
+                case 5:
+                    return;
+            }
+        }
+    }
+}
